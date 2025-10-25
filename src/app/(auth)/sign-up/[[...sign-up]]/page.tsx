00eab29a193997419ffbf336d5e0a5d0de9dd611
Mohamed780050 <mohamed39omer@gmail.com>
2025-10-25 17:44:41 +0300

    added Login functionaltiy

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { SignUp } from "@clerk/nextjs";
+import { useTheme } from "next-themes";
+import { dark } from "@clerk/themes";
+
+export default function Page() {
+  const { resolvedTheme } = useTheme();
+  return (
+    <SignUp
+      appearance={{
+        baseTheme: resolvedTheme === "dark" ? dark : undefined,
+      }}
+    />
+  );
+}
