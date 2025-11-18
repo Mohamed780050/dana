90bf0a2631afb55052a632a7e723a6ab2041ead5
Mohamed780050 <mohamed39omer@gmail.com>
2025-11-18 21:42:53 +0200

    finished public restaurant

--- src/app/restaurantMenu/[id]/page.tsx
+++ src/app/restaurantMenu/[id]/page.tsx
@@ -1,9 +1,15 @@
 import Restaurant from "@/features/public restaurant/_components/Restaurant";
+import { db } from "@/lib/db";
 import { getRestaurantDetailsForPublicRoute } from "@/lib/restaurant";
 
 export default async function page({ params }: { params: { id: string } }) {
   const { id } = await params;
   const restaurant = await getRestaurantDetailsForPublicRoute(id);
+  if (!restaurant) throw new Error("Not found");
+  await db.cardsStatus.update({
+    where: { userId: restaurant.userId },
+    data: { views: { increment: 1 } },
+  });
   return (
     <div>
       {restaurant ? (
